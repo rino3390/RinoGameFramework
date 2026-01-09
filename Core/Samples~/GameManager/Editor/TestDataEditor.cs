acb989f646c568dd6ba3e2e9766b48b44682a50c
rino <sherikawaii3390@gmail.com>
2026-01-09 19:59:23 +0800

    [新增] GameManager Sample 範例

---
+++ Core/Samples~/GameManager/Editor/TestDataEditor.cs
@@ -0,0 +1,16 @@
+#if UNITY_EDITOR
+using Rino.GameFramework.Core.GameManagerBase.EditorBase;
+
+namespace Rino.GameFramework.Sample.GameManager.Editor
+{
+    /// <summary>
+    /// 測試用資料 Editor 視窗
+    /// </summary>
+    public class TestDataEditor : CreateNewDataEditor<TestData>
+    {
+        protected override string DataRoot => "Data/Sample/TestData";
+
+        protected override string DataTypeLabel => "測試資料";
+    }
+}
+#endif
